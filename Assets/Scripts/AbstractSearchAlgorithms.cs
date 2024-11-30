518438bb57c301d0a259f2ff42ecfb341e4211ab
Can Çalışkan <canc953@gmail.com>
2024-11-30 22:36:01 +0300

    Algorithm ile uğraştım + 4 tarafa giden Skill alg. ekledim
    Algorithm içinde biraz uğraştım + 4 tarafa giden Skilller için alg. ekledim ama test etmedim henüz. Path bulma için alg. yazmaya çalıştım ama yarıda kaldı.

--- Assets/Scripts/AbstractSearchAlgorithms.cs
+++ Assets/Scripts/AbstractSearchAlgorithms.cs
@@ -1,24 +1,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract  class AbstractSearchAlgorithms // template method gibi yazayım dedim de haybeye yazıyorum. Elimizde birkaç tane algoritma olunca yazsam çok daha iyi olacak.
+public abstract class AbstractSearchAlgorithms // template method gibi yazayım dedim de haybeye yazıyorum. Elimizde birkaç tane algoritma olunca yazsam çok daha iyi olacak.
                                                 //kafana göre return veriyorum çünkü şimdi bunu yazarken ve büyük ihtimalle uymayacak 
 {
-    virtual public HashSet<Vector3> startAlgorithm(Grid grid, int deepening_count)
+    private HashSet<Vector3> tempHashSet = new HashSet<Vector3>();
+    
+    virtual public HashSet<Vector3> startAlgorithm(Grid input_grid, int deepening_count)
     {
-        HashSet<Vector3> tempHashSet = new HashSet<Vector3>();
-        algorithm(grid, tempHashSet,  deepening_count);
+        algorithm(input_grid, tempHashSet,  deepening_count);
         return tempHashSet;
     }
 
-    virtual protected void algorithm(Grid grid, HashSet<Vector3> givenHashSet, int deepening_count)
+    virtual protected void algorithm(Grid input_grid, HashSet<Vector3> givenHashSet, int deepening_count)
     {
         baseCase();
         bodyPart();
         iterativePart();
     }
 
-    virtual protected Vector3 baseCase() {return Vector3.zero;}
+    virtual protected void baseCase(){}
     virtual protected void bodyPart(){}
     virtual protected void iterativePart(){}
 }
