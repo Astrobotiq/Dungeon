dae3aee524324398f1e7f6e4581925a9183b1a80
Astrobotiq <105081753+Astrobotiq@users.noreply.github.com>
2024-12-17 16:54:53 +0300

    SkillEffect Interface written

---
+++ Assets/Scripts/Skill/SkillEffect/ISkillEffect.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public abstract class ISkillEffect : MonoBehaviour
+{
+    //After instantiating Effect Object we should call this function
+    //This function will call directly apply effect in close combat and others will discussed
+    public abstract void StartMoving(Grid targetGrid);
+
+    //As name suggest effect should apply the target here.
+    //This maybe called from onTrigger Enter or startMoving function
+    public abstract void ApplyEffect(Grid targetGrid);
+}
