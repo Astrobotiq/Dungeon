5c138a3bae7153a4e0d242d9e4ee39f16e692cb9
Can Çalışkan <canc953@gmail.com>
2025-02-03 18:11:52 +0300

    Little changes on skills and algorithms

--- Assets/Scripts/Skill/RotateSkill/RotateSkillScript.cs
+++ Assets/Scripts/Skill/RotateSkill/RotateSkillScript.cs
@@ -5,31 +5,32 @@ using UnityEngine;
 
 public class RotateSkillScript : ISkillEffect
 {
-    [SerializeField,Range(0.1f,5)] float jumpPower = 1;
-    [SerializeField, ReadOnly] int jumpNumber = 1;
-    [SerializeField] float jumpDuration = 1;
-    public int DamageAmount;
-    [SerializeField] Grid _target;
+    [SerializeField,Range(0.1f,5)] 
+    float jumpPower = 1;
+    
+    [SerializeField, ReadOnly]
+    int jumpNumber = 1;
+    
+    [SerializeField] 
+    float jumpDuration = 1;
     
     public override void StartMoving(Grid targetGrid) {
-        _target = targetGrid;
         transform.DOJump(targetGrid.gameObject.transform.position, jumpPower, jumpNumber, jumpDuration);
         StartCoroutine(Timer());
 
         IEnumerator Timer()
         {
             yield return new WaitForSeconds(jumpDuration);
-            ApplyEffect();
-            Destroy(gameObject);
+            ApplyEffect(targetGrid);
         }
     }
 
-    public override void ApplyEffect(Grid targetGrid = null) {
+    public override void ApplyEffect(Grid targetGrid) {
 
         if (targetGrid.gameObject && targetGrid.GridObject && targetGrid.GridObject.GetComponent<IRotatable>())
         {
             targetGrid.GridObject.GetComponent<IRotatable>().Rotate(Vector3.up, 90); //This will do not rotate because Rotate didn't overriden by
         }
-        
+        Destroy(gameObject);
     }
 }
