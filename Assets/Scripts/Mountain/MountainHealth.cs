8af9e5eb3ec606e69e77a135c32a6034bb7486dd
Astrobotiq <105081753+Astrobotiq@users.noreply.github.com>
2025-05-20 03:17:59 +0300

    ErenLevel

--- Assets/Scripts/Mountain/MountainHealth.cs
+++ Assets/Scripts/Mountain/MountainHealth.cs
@@ -2,10 +2,16 @@ using UnityEngine;
 
 public class MountainHealth : IHealth
 {
+    [SerializeField] GameObject particle;
     public override void TakeDamage(int damage, bool willPush)
     {
-        EventManager.Instance.InvokeOnMountainDestroyed();
         
+        if (currentHealth - damage <= 0)
+        {
+            EventManager.Instance.InvokeOnMountainDestroyed();
+            var particleGO = Instantiate(this.particle, transform, false);
+            Destroy(particleGO,1f);
+        }
         base.TakeDamage(damage);
     }
 }
